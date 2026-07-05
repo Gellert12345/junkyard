286090079ed17ebfd48ae0d9a10c18a8dcd7404c
Gellért <gellertkecskes@gmail.com>
2026-07-05 14:59:53 +0200

    idk

---
+++ src/main/java/com/example/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+//impotz minden minz react-ba
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+//ettol lesz restApi-s backend controller file mint a javascirpt-be
+@RestController
+
+public class HelloController {
+    //folyamatosan figyeli
+    @GetMapping("/") //api end point
+    public String hello() {
+        return "Hello word";
+    }
+}
---
+++ src/main/java/com/example/controllers/OrderController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+//import like react
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+    @GetMapping("/order")
+    public String order(@RequestParam String item) {
+        return "Your order for " + item + "has been received"; 
+
+    }
+}
