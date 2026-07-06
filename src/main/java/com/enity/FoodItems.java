f92c6364633514991adb12940207d52b9bd1cd77
Gellért <gellertkecskes@gmail.com>
2026-07-06 16:14:35 +0200

    getter and setter applyed

--- src/main/java/com/enity/FoodItems.java
+++ src/main/java/com/enity/FoodItems.java
@@ -6,11 +6,30 @@ import jakarta.persistence.Id;
 
 
 @Entity
-public class FoodItems {3
+public class FoodItems {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private int id;
+    private int id; //id => (primary key)
     private String name;
     private double price;
     //getters and setters apply
+    public int getId() {
+        return id;
+    }
+    public void setId(int,id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
 }
