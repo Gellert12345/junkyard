8de2f297ebc0eaa84cab4468a88e9214841b12bc
Gellért <gellertkecskes@gmail.com>
2026-07-06 16:18:05 +0200

    apply comment for help

--- src/main/java/com/enity/FoodItems.java
+++ src/main/java/com/enity/FoodItems.java
@@ -4,14 +4,20 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+//Annotáciok == @-Al kezdődő dolgok!!
 
 @Entity
 public class FoodItems {
-    @Id
+    @Id //ez jelzi hogy az Id a primary key
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id; //id => (primary key)
     private String name;
     private double price;
+    public FoodItems(int id,String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
     //getters and setters apply
     public int getId() {
         return id;
