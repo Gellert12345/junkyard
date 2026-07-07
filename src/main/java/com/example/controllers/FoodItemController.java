4ffd15d7266e0c6838e10508cf14c8404a5d3454
Gellért <gellertkecskes@gmail.com>
2026-07-07 17:28:37 +0200

    add comments for the Controller http request section

--- src/main/java/com/example/controllers/FoodItemController.java
+++ src/main/java/com/example/controllers/FoodItemController.java
@@ -1,7 +1,10 @@
 package com.example.controllers;
 
-@RestController
+import org.springframework.web.bind.annotation.RestController //to can req res stb
 
+@RestController
+@RequestMapping("/fooditem")
+//http request and response
 public class FoodItemController {
 
 }
