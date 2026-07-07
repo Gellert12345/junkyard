d0bebedc7b225a00cf094b1380d2337ab3e96692
Gellért <gellertkecskes@gmail.com>
2026-07-07 11:54:40 +0200

    addigng comments code examlex for the backendDB

--- src/main/java/com/repository/FoodItemRepository.java
+++ src/main/java/com/repository/FoodItemRepository.java
@@ -1,8 +1,10 @@
-package com.example.demo;
+package com.example.demo; //code melyik mappaba lakik
 
-import org.springframework.data.jpa.repository.JpaRepository
-import com.example.demo.entity.FoodItems
+import org.springframework.data.jpa.repository.JpaRepositoryn //importaljak egy spring libaryt adabazisokhoz
+import com.example.demo.entity.FoodItems //importlajuka az egyik class schemat egy másik codebol
 
+// 4. Egy adatbázis-kezelő interfész (Repository), amely a Spring Data JPA-ból örökli a CRUD műveleteket.
+// A kacsacsőrben megadjuk az entitás osztályt (FoodItems) és annak elsődleges kulcsának típusát (Long).
 public interface FoodItemRepository extends JpaRepository<FoodItems, Long>{
 
 }
