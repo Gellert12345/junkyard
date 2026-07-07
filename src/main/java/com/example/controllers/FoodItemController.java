30caa01cc30e43d627a9920ef11a9ca9f47578d1
Gellért <gellertkecskes@gmail.com>
2026-07-07 17:34:11 +0200

    last commit before the work session end

--- src/main/java/com/example/controllers/FoodItemController.java
+++ src/main/java/com/example/controllers/FoodItemController.java
@@ -1,10 +1,15 @@
 package com.example.controllers;
 
 import org.springframework.web.bind.annotation.RestController //to can req res stb
+import org.springframework.web.bind.annotation.RequestMapping  //so i can list the api endpoint
+
 
 @RestController
-@RequestMapping("/fooditem")
+@RequestMapping("/api/foods")
 //http request and response
 public class FoodItemController {
+    public FoodItemController(FoodItemRepository FoodItemRepository){
+        this.FoodItemRepository = FoodItemRepository;
+    }
 
 }
