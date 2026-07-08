7a107382045a5264a3667e08209f81daa1ae0ef8
Gellért <gellertkecskes@gmail.com>
2026-07-08 13:03:20 +0200

    bug fix small vs big capital word misspelling  variable declarition

--- src/main/java/com/example/controllers/FoodItemController.java
+++ src/main/java/com/example/controllers/FoodItemController.java
@@ -1,15 +1,17 @@
 package com.example.controllers;
 
-import org.springframework.web.bind.annotation.RestController //to can req res stb
-import org.springframework.web.bind.annotation.RequestMapping  //so i can list the api endpoint
-
+import org.springframework.web.bind.annotation.RestController; //to can req res stb
+import org.springframework.web.bind.annotation.RequestMapping;  //so i can list the api endpoint
+import com.example.repository.FoodItemRepository;
 
 @RestController
 @RequestMapping("/api/foods")
 //http request and response
 public class FoodItemController {
-    public FoodItemController(FoodItemRepository FoodItemRepository){
-        this.FoodItemRepository = FoodItemRepository;
+    private final FoodItemRepository foodItemRepository;
+
+    public FoodItemController(FoodItemRepository foodItemRepository){
+        this.foodItemRepository = foodItemRepository;
     }
 
 }
